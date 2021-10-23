ecee7c5b60ee72b5a2b90e26b4b55fe27777646e
vantoanvtt <vantoanvtt37@gmail.com>
2021-10-23 17:07:00 +0700

    fix port

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const route = require('./routes');
 // connect to db
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 // route init
 route(app);
